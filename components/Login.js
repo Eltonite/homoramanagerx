ced81175b2d86f3ed37857e3dcd6ffce99d0468d
Eltonite <eltoniteonblockchain@gmail.com>
2022-01-06 16:32:16 -0600

    Setup basic Next.js app with Moralis

---
+++ components/Login.js
@@ -0,0 +1,35 @@
+import Image from "next/image"
+import wallpaper from "/images/backgroundragna.png"
+import ragna from "/images/chibi-ragna.png"
+import { useMoralis } from 'react-moralis'
+ 
+
+function Login() {
+
+  const { authenticate } = useMoralis()
+
+
+  return (
+    <div className="bg-blue-900 text-white relative">
+
+      <h1 className="text-center">Login Screen</h1>
+
+      <div className="flex flex-col absolute z-50 w-full items-center justify-center h-4/6 gap-4">
+        <Image src={ragna} height={200} width={200}/>
+
+        <button className="bg-blue-500 rounded-lg p-4 font-bold animate-bounce" onClick={authenticate}>LOGIN</button>
+      </div>
+
+      <div className="w-full h-screen opacity-20">
+        <Image 
+          src={wallpaper} 
+          alt="Picture of the author"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Login
